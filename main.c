0a911a20c26123b48d650aa195c34a6340de1a15
antoniojorgefernandes <antoniojorgefernandes@ua.pt>
2019-12-20 16:53:26 +0000

    mmmd

--- main.c
+++ main.c
@@ -10,12 +10,15 @@ typedef struct node
 {
     long word_pos;
     long word_num;
+    long word_count;
     char word[64];
     long first_pos;
     long current_pos;
     long max_distance;
     long min_distance;
     long med_distance;
+    long last_pos;
+    long prev_pos;
     FILE *fp;
     struct node_t *next;
 } node_t;
@@ -42,7 +45,6 @@ unsigned int hash_function(const char *str, unsigned int s)
     crc = 0xAED02019u; // initial value (chosen arbitrarily)
     while (*str != '\0')
         crc = (crc >> 8) ^ table[crc & 0xFFu] ^ ((unsigned int)*str++ << 24);
-    printf("HASHCODE:::::::: %d\n", crc % s);
     return crc % s;
 }
 /*----------------------------------------------*/
@@ -57,16 +59,9 @@ void printHashTable()
         {
             continue;
         }
-        for (;;)
-        {
-            printf(" WORD ::: %s\n WORD_POS ::: %ld\n WORD_NUM ::: %ld\n CURRENT POS ::: %ld\n MAX DISTANCE ::: %ld\n MIN DISTANCE ::: %ld\n AVG DISTANCE ::: %ld\n", current->word, current->word_pos, current->word_num, current->current_pos, current->max_distance, current->min_distance, current->med_distance);
-            current = current->next;
-            
-            if (current == NULL)
-                break;
-        }
+        printf(" WORD ::: %s\n WORD_COUNT ::: %ld\n FIRST_POS ::: %ld\n LAST POS ::: %ld\n MAX DISTANCE ::: %ld\n MIN DISTANCE ::: %ld\n AVG DISTANCE ::: %ld\n", current->word,current->word_count, current->first_pos,  current->last_pos, current->max_distance, current->min_distance, current->med_distance);
         printf("\n");
-    }
+        }
 }
 
 /*----------------------------------------------*/
@@ -122,16 +117,18 @@ void insert(node_t *word_info)
 
     long current_distance;
 
-    node_t *node = malloc(sizeof(node_t));
-    node = word_info;
-
-    printf(":::::::: %s\n", node->word);
-    int hcode = hash_function(node->word, 37);
+    int hcode = hash_function(word_info->word, SIZE);
 
     if (hashTable[hcode] == NULL)
     {
-        hashTable[hcode] = node;
-        return;
+        hashTable[hcode] = word_info;
+        hashTable[hcode]->first_pos=word_info->current_pos-strlen(word_info->word);
+        hashTable[hcode]->last_pos=hashTable[hcode]->first_pos;
+        hashTable[hcode]->word_count=1;
+        hashTable[hcode]->max_distance=0;
+        hashTable[hcode]->min_distance=999999999;
+        hashTable[hcode]->med_distance=0;
+        hashTable[hcode]->prev_pos=word_info->current_pos;
     }
     else
     {
@@ -139,39 +136,30 @@ void insert(node_t *word_info)
         current = hashTable[hcode];
         // se as palavras forem as mesmas (e hashcode igual)
         // neste caso apenas temos de atualizar as informacoes do node
-        if (strcmp(current->word, node->word) == 0)
+        if (strcmp(current->word, word_info->word) == 0)
         {
-            printf("same word\n");
-            /*pos, num, cuurentpos, depois-> distances*/
-            current_distance = node->word_pos - current->word_pos;
-
-            current->word_num = node->word_num;
-            current->word_pos = node->word_pos;
-            current->current_pos = node->current_pos;
-
-            if (current_distance > current->max_distance)
-            {
-                current->max_distance = current_distance;
-            }
+            /*pos, num, currentpos, depois-> distances*/
+            current->word_count++;
+            current->med_distance = (current->med_distance+ (word_info->current_pos - current->prev_pos))/(current->word_count-1);
+            current_distance = word_info->word_pos - current->word_pos;
+			current->last_pos=word_info->current_pos-strlen(word_info->word);
+            current->word_num = word_info->word_num;
+            current->word_pos = word_info->word_pos;
             if (current_distance < current->min_distance)
             {
                 current->min_distance = current_distance;
             }
-            current->med_distance = current->med_distance + (current_distance - current->med_distance) / (current->word_num + 1);
+            if(current->current_pos> current->last_pos)
+            {
+				current->last_pos=current->current_pos;
+			}
+			current->max_distance = current-> last_pos - current->first_pos;
+            current->current_pos = word_info->current_pos;
             //media = media + (valoratual - media)/(num valores metidos +1)
-            return;
         }
         // se as palavras forem diferentes (e hashcode igual)
         // neste caso temos de adicionar um novo node com a nova palavra
-        else
-        {
-            while (current->next)
-            {
-                current = current->next;
-            }
-            current = node;
-        }
-        return;
+
     }
 }
 /*----------------------------------------------*/
@@ -179,16 +167,24 @@ void insert(node_t *word_info)
 int main(int argc, char const *argv[])
 {
     node_t *nd = malloc(sizeof(node_t));
-    open_text_file("/home/lucas/Desktop/p2AED/teste.txt", nd);
+    open_text_file("teste.txt", nd);
 
     while (read_word(nd) == 0)
     {
-        printf("contr ::: %d\n", c);
-        //printf("%s->    Current Position %ld\n\t First Postion %ld\n\t Word Num %ld\n\t Word Pos %ld\n", nd->word, nd->current_pos, nd->first_pos, nd->word_num, nd->word_pos);
-        insert(nd);
+		node_t *pq = malloc(sizeof(node_t));
+		strcpy(pq->word,nd->word);
+		pq->word_pos = nd->word_pos;
+		pq->word_num = nd->word_num;
+		pq->first_pos= nd->first_pos;
+		pq->last_pos = nd->last_pos;
+		pq->current_pos = nd->current_pos;
+		pq->max_distance= nd->max_distance;
+		pq->min_distance = nd->min_distance;
+		pq->med_distance = nd->med_distance;
+        insert(pq);
     }
     close_text_file(nd);
-    printHashTable();
+   printHashTable();
 
     return 0;
 }
